0195ebcd86a53875d007aa0732eadd992fe201f3
kyd <kydculx@gmail.com>
2026-02-19 01:48:32 +0900

    fix: switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Lobby from './pages/Lobby';
 import Player from './pages/Player';
 
